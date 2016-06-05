10ed377f822bde17ecea64b67d781237cf51a3e0
loften <lcw93663469@163.com>
2016-06-05 22:16:32 +0800

    handler使用

---
+++ app/src/main/java/com/loften/samples/HandlerTest/HandlerActivity.java
@@ -0,0 +1,15 @@
+package com.loften.samples.HandlerTest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.loften.samples.R;
+
+public class HandlerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_handler);
+    }
+}
